808ecb100fc31a42997bd3a42179cb9f8b05b15b
theshane345 <56794959+theshane345@users.noreply.github.com>
2019-10-30 19:52:57 +0000

    splash_music

---
+++ Assets/Load.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LevelLoad", 2f);
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void LevelLoad()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
