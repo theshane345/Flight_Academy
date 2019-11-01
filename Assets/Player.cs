3ee647a991f837f8d984df02aa0246be1be9a103
theshane345 <56794959+theshane345@users.noreply.github.com>
2019-11-01 22:11:05 +0000

    Flying_start

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRangeAccross = 2f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRangeUp = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +20,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        LeftRight();
+        UpDown();
+    }
+
+    private void LeftRight()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float Raw = transform.localPosition.x + xOffset;
+        float ClampPos = Mathf.Clamp(Raw, -xRangeAccross, xRangeAccross);
+        transform.localPosition = new Vector3(ClampPos,transform.localPosition.y,  transform.localPosition.z);
+    }
+
+    private void UpDown()
+    {
+        float xFly = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetUp = xFly * xSpeed * Time.deltaTime;
+        float RawUp = transform.localPosition.y + yOffsetUp;
+        float ClampPosUp = Mathf.Clamp(RawUp, -yRangeUp, yRangeUp);
+        transform.localPosition = new Vector3(transform.localPosition.x, ClampPosUp, transform.localPosition.z);
     }
 }
