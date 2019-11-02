5c0ffc2e0ef18729fffb2a7427053e56b23cf0bd
theshane345 <56794959+theshane345@users.noreply.github.com>
2019-11-02 18:00:56 +0000

    flight_done_laser_start

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,14 +8,22 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In ms^-1")] [SerializeField] float xRangeAccross = 2f;
-    [Tooltip("In ms^-1")] [SerializeField] float yRangeUp = 2f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRangeAccross = 6f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRangeUp = 4.5f;
 
+    [SerializeField] float PitchPos = -4.5f;
+    [SerializeField] float ControlPitch = 14.5f;
+
+    [SerializeField] float YawPos = 4.5f;
+    [SerializeField] float ControlRoll = -40.5f;
+    
+
+    float yThrow, xThrow;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,11 +31,28 @@ public class Player : MonoBehaviour
     {
         LeftRight();
         UpDown();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float picthDueToControl = yThrow * ControlPitch;
+        float pitchDueToPos = transform.localPosition.y * PitchPos;
+        float pitch = pitchDueToPos + picthDueToControl;
+
+
+        float yaw = transform.localPosition.x * YawPos;
+
+
+        float roll = xThrow * ControlRoll;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void LeftRight()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float Raw = transform.localPosition.x + xOffset;
         float ClampPos = Mathf.Clamp(Raw, -xRangeAccross, xRangeAccross);
@@ -35,8 +61,8 @@ public class Player : MonoBehaviour
 
     private void UpDown()
     {
-        float xFly = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffsetUp = xFly * xSpeed * Time.deltaTime;
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetUp = yThrow * xSpeed * Time.deltaTime;
         float RawUp = transform.localPosition.y + yOffsetUp;
         float ClampPosUp = Mathf.Clamp(RawUp, -yRangeUp, yRangeUp);
         transform.localPosition = new Vector3(transform.localPosition.x, ClampPosUp, transform.localPosition.z);
