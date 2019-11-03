59b1850a6bf696d6031f83fdd80af76e68c3184c
theshane345 <56794959+theshane345@users.noreply.github.com>
2019-11-03 15:03:30 +0000

    death_start

---
+++ Assets/cScripta/CollisionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject Explosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeath();
+    }
+
+    void StartDeath()
+    {
+        SendMessage("OnPlayerDeath");
+        Explosion.SetActive(true);
+        Invoke("load", levelLoadDelay);
+    }
+
+
+    void load() {
+        SceneManager.LoadScene(1);
+
+    }
+}
---
+++ Assets/cScripta/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float Speed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRangeAccross = 6f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRangeUp = 2.5f;
+
+    [Header("Screen Position")]
+    [SerializeField] float PitchPos = -4.5f;
+    [SerializeField] float YawPos = 4.5f;
+
+
+    [Header("Control Throw")]
+    [SerializeField] float ControlPitch = 14.5f;
+    [SerializeField] float ControlRoll = -40.5f;
+    
+    
+
+    float yThrow, xThrow;
+    bool isDead = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead) {
+            LeftRight();
+            UpDown();
+            ProcessRotation();
+        }
+
+    }
+
+    void OnPlayerDeath()
+    {
+        isDead = false;
+        
+    }
+    
+
+    private void ProcessRotation()
+    {
+        float picthDueToControl = yThrow * ControlPitch;
+        float pitchDueToPos = transform.localPosition.y * PitchPos;
+        float pitch = pitchDueToPos + picthDueToControl;
+
+
+        float yaw = transform.localPosition.x * YawPos;
+
+
+        float roll = xThrow * ControlRoll;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void LeftRight()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float Raw = transform.localPosition.x + xOffset;
+        float ClampPos = Mathf.Clamp(Raw, -xRangeAccross, xRangeAccross);
+        transform.localPosition = new Vector3(ClampPos,transform.localPosition.y,  transform.localPosition.z);
+    }
+
+    private void UpDown()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetUp = yThrow * Speed * Time.deltaTime;
+        float RawUp = transform.localPosition.y + yOffsetUp;
+        float ClampPosUp = Mathf.Clamp(RawUp, -yRangeUp, yRangeUp);
+        transform.localPosition = new Vector3(transform.localPosition.x, ClampPosUp, transform.localPosition.z);
+    }
+}
